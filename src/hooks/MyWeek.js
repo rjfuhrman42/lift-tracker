ab2fd74e104c9806f8f8291966d8f3cac7e042dd
Reid Fuhrman <rjfuhrman42@students.tntech.edu>
2020-03-11 14:49:58 -0500

    added a weekly calendar component

--- src/hooks/MyWeek.js
+++ src/hooks/MyWeek.js
@@ -1,8 +1,16 @@
 import React from "react"
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import '@fullcalendar/core/main.css'
 
 function MyWeek() {
     return (
-        <h1>My Week</h1>
+        <>
+            <FullCalendar defaultView="dayGridWeek" 
+            header={{left:   'title',center: '',right:  'today prev,next'}}
+            titleFormat={{day: 'numeric'}}
+            plugins={[ dayGridPlugin ]} />
+        </>
     )
 }
 
