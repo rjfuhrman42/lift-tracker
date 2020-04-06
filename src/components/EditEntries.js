ae8686e51ec5f4eb901051059456cf179a767228
Reid Fuhrman <rjfuhrman42@students.tntech.edu>
2020-04-06 14:16:37 -0500

    edit section is now working to the best of my knowledge

--- src/components/EditEntries.js
+++ src/components/EditEntries.js
@@ -5,27 +5,39 @@ import firebase from "firebase"
 function EditEntries(props) {
 
 
-    var [dates, setDates] = useState()
-    var [entries, setEntries] = useState()
-    var ref = firebase.database().ref(`/users/${props.uid}/logs`)   // get a reference to the logs section of data in the firebase RTDB 
+    var [dates, setDates] = useState([])
+    var [ref, setRef] = useState(firebase.database().ref(`/users/${props.uid}/logs`))
+
 
     useEffect(() => {
-        let test =[]
-        ref.on('value', (snapshot) => {
-            snapshot.forEach(date => {
-                console.log(date.val())
-                test.push(<EntryList items={date}/>)
-            })
-            setDates([...test])
-            setEntries(snapshot.val())
-        }) 
-        return () => ref.off()   
+        getLogs()
     }, [])
+
+
+    function getLogs() {
+        ref.on('value',  (snapshot) => {
+            let items = []
+            snapshot.forEach(date => {
+                items.push(
+                    <li key={date.key}>
+                        <EntryList items={date} deleteEntry={deleteEntry} />
+                    </li>)
+
+            })
+            setDates([...items])
+        })
+    }
+
+    function deleteEntry(key, date) {
+        var refer = firebase.database().ref(`/users/${props.uid}/logs/${date}`).child(key)
+        refer.remove()
+    }
 
     return (
         <div className="edit-page">
             <ul>
                 {dates}
+                {console.log(dates)}
             </ul>
         </div>
     )
--- src/components/EntryList.js
+++ src/components/EntryList.js
@@ -1,28 +1,59 @@
 import React, { useState, useEffect } from "react"
 
-function EntryList({items}){
+function EntryList({items, deleteEntry, reference}){
 
     let [exercises, setExercises] = useState([])
+    var [entries, setEntries] = useState([])
     let [show, setShow] = useState(false);
+    let [ref, setRef] = useState(reference)
 
     useEffect(() => {
+        let test = []
         items.forEach(item => {
-            // console.log(item.val())
-            setExercises(prevExercises => [...prevExercises, <li>{item.val().exercise}</li>])
+            let entry = item.val()
+            console.log(item.val())
+            if(item.val().restDay) test.push(
+                <li key={item.ref.key}>
+                    <h1>Rest day</h1>
+                    <button className="del-btn"                                                         // Rest Day
+                    onClick={() => deleteItems(item.ref.key, items.key)}>Delete Entry?</button> 
+                </li>
+            )
+            else
+            test.push(
+                <li key={item.ref.key}>
+                    <b>{entry.exercise}</b>
+                    <ul class="entry">
+                        <li>Sets: {entry.sets}</li> 
+                        <li>Reps: {entry.reps}</li>                                                     {/* Regular Entry*/}
+                        <li>Weight: {entry.weight} {entry.measurement}</li>
+                    </ul>
+                    <button className="del-btn"
+                    onClick={() => deleteItems(item.ref.key, items.key)}>Delete Entry?</button> 
+                </li>
+            )
+            setEntries(prevEntries => [...prevEntries, entry])
         })
+        setExercises([...test])
     }, [])
 
-    function handleClick(e) {
+    function deleteItems(refKey, key) {
+        deleteEntry(refKey, key)
+        
+        setExercises(prev => {
+            let newArr = prev.filter(item => item.key !== refKey)
+            
+            return newArr
+        })
     }
 
     return (
         <li 
             className={`entry-item ${show ? "show" : ""}`} 
             key={items.key} 
-            onClick={() => setShow(prev => !prev)}
             >
 
-            {items.key}
+            <h3 onClick={() => setShow(prev => !prev)}>{items.key}</h3>
             
             <ul className="entry-list">
                 {exercises}
